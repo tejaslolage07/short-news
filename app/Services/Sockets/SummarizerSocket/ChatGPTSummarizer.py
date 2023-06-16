656b7f76b6db23dcb7b77bcdd9e1edcf838ed638
Aadeesh11 <asoswal@gmail.com>
2023-06-16 12:34:58 +0530

    Cleanup: Changed file locations

--- app/Services/Sockets/SummarizerSocket/ChatGPTSummarizer.py
+++ app/Services/Sockets/SummarizerSocket/ChatGPTSummarizer.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+#Just a dummy prompt, the proper one is added in the chatGPT integration PR
 def _generate_prompt(body):
     return "Summarize the news article in 60 japanese words:\n" + body
 
