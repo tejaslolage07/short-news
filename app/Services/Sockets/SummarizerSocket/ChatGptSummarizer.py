e5c5c97c4e9f0ff88c50af0719784c9cc6c7e650
Aadeesh11 <asoswal@gmail.com>
2023-06-19 08:29:08 +0530

    fix: Add self param to all class methods.

--- app/Services/Sockets/SummarizerSocket/ChatGptSummarizer.py
+++ app/Services/Sockets/SummarizerSocket/ChatGptSummarizer.py
@@ -11,10 +11,10 @@ class ChatGptSummarizer:
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
     #Just a dummy prompt, the proper one is added in the chatGPT integration PR
-    def _generate_prompt(body):
+    def _generate_prompt(self, body):
         return "Summarize the news article in 60 japanese words:\n" + body
 
-    def summarize(articleBody) -> str:
+    def summarize(self, articleBody) -> str:
         return 'Dummy summarized data'
 
 
