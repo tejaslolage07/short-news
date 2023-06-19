5c53162a5a9902f0c3adaa22b60cf92f7f959901
Aadeesh11 <asoswal@gmail.com>
2023-06-19 13:05:41 +0530

    feat: Implement article-writer.

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -5,29 +5,25 @@ namespace Database\Factories;
 use App\Models\NewsWebsite;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class ArticleFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
             'short_news' => '',
             'headline' => fake()->text(),
-            'news_website_id' => NewsWebsite::all()->random()->id,
+            'news_website_id' => NewsWebsite::factory(),
             'author' => fake()->text(),
             'article_url' => fake()->url(),
             'image_url' => fake()->url(),
             'article_s3_filename' => fake()->text(),
             'published_at' => fake()->dateTime(),
             'fetched_at' => fake()->dateTime(),
-            'source' => fake()->randomElement(['api', 'scraper']),
+            'source' => fake()->randomElement(['bingApi', 'newsDataIoApi', 'scraper']),
+            'country' => fake()->randomElement(['jp', 'us', 'in']),
+            'language' => fake()->randomElement(['ja', 'en']),
+            'category' => fake()->randomElement(['World', 'Business', 'Technology', 'Entertainment', 'Sports', 'Science', 'Health']),
+            'keywords' => fake()->randomElement([json_encode(['keyword1', 'keyword2', 'keyword3']), json_encode(['keyword4', 'keyword5', 'keyword6'])]),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,13 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        \App\Models\NewsWebsite::factory(5)->create();
-        \App\Models\Article::factory(10)->create();
+        (new NewsWebsiteSeeder())->run();
+        (new ArticleSeeder())->run();
     }
 }
