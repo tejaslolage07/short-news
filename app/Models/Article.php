0d8c3559bea550691d28fd2407565b83fb63f6d7
Aadeesh11 <asoswal@gmail.com>
2023-06-19 13:05:41 +0530

    feat: Implement article-writer.

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -9,29 +9,21 @@ class Article extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'short_news',
         'article_s3_filename',
+        'country',
+        'language',
+        'category',
+        'keywords',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<int, string>
-     */
     protected $casts = [
         'published_at' => 'datetime',
         'fetched_at' => 'datetime',
+        'keywords' => 'array',
     ];
 
-    /**
-     * Get the news website that owns the article.
-     */
     public function newsWebsite()
     {
         return $this->belongsTo(NewsWebsite::class);
