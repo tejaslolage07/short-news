f66a568031b051b30adb3fe6aa21dc6a38dcc99c
Aadeesh11 <asoswal@gmail.com>
2023-06-20 07:50:52 +0530

    Add NewsWebsite model, controller, factory. Upd Article model with Foreign Key.

---
+++ app/Models/NewsWebsite.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NewsWebsite extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'website',
+    ];
+
+    /**
+     * Get the articles for the news website.
+     */
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
+}
