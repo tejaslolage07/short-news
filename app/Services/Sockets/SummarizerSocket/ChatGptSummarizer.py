2cbde2617ef5aa17b1ec59f76f6280141832478e
Aadeesh11 <asoswal@gmail.com>
2023-06-21 07:55:37 +0530

    fix: Model=classVar, MaxInputTokens can be passed.

--- app/Services/Sockets/SummarizerSocket/ChatGptSummarizer.py
+++ app/Services/Sockets/SummarizerSocket/ChatGptSummarizer.py
@@ -7,22 +7,32 @@ load_dotenv()
 
 class ChatGptSummarizer:
     MAX_INPUT_TOKENS = None
-    MODEL=None
+    def __init__(self):
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        self.MAX_INPUT_TOKENS = int(os.getenv("MAX_INPUT_TOKENS"))
 
-    def _tokenized_prompt(self, text):
+    def _generate_prompt(self,body):
+        return f"""
+    Summarize the news article below that is delimited by triple quotes.\
+    Respond in Japanese and in no more than 60 words.
+
+    Article: ```{body}```
+    """
+
+    def _tokenizedPrompt(self, text):
         #get the number of tokens in the article body
         encoding = tiktoken.get_encoding("cl100k_base")
         return encoding.encode(text)
 
-    def _format_prompt(self, prompt:str):
-        #Strip the prompt of whitespaces.
-        return prompt.strip()
+    def _prepare_body(self, articleBody:str):
+        #Strip the article body.
+        return articleBody.strip()
 
 
     def _api_call(self, prompt, max_tokens:int, temperature:float):
         messages = [{"role": "user" , "content": prompt}]
         response = openai.ChatCompletion.create(
-            model=self.MODEL,
+            model="gpt-3.5-turbo",
             messages=messages,
             #lower the temperature to reduce randomness
             temperature=temperature,
@@ -30,18 +40,13 @@ class ChatGptSummarizer:
         )
         return response
 
-    def summarize(self, chatGptPrompt:str) -> str:
+    def summarize(self, articleBody:str) -> str:
         try:
-            #get the environment variables
-            openai.api_key = os.getenv("OPENAI_API_KEY")
-            self.MODEL=os.getenv("GPT_MODEL")
-            self.MAX_INPUT_TOKENS = int(os.getenv("MAX_INPUT_TOKENS"))
-
-            textPrompt = self._format_prompt(chatGptPrompt)
-            # textPrompt = self._generate_prompt(articleBody)
+            articleBody = self._prepare_body(articleBody)
+            textPrompt = self._generate_prompt(articleBody)
             print(textPrompt)
 
-            tokenArray = self._tokenized_prompt(textPrompt)[0:self.MAX_INPUT_TOKENS]
+            tokenArray = self._tokenizedPrompt(textPrompt)[0:self.MAX_INPUT_TOKENS]
             num_tokens = len(tokenArray)
 
             response = self._api_call(prompt=textPrompt, max_tokens=min(300, 2048-num_tokens, num_tokens), temperature=0.0)
@@ -52,4 +57,6 @@ class ChatGptSummarizer:
             print("Error in summarizer: ", e)
             raise e
         
+#Call this function with the article body as the argument
+#ChatGptSummarizer().summarize("「R―1ぐらんぷり2018」王者で、視覚障がいを持つピン芸人・濱田祐太郎（33）が16日、自身のツイッターを更新。秘書だった男性を殴ったことを明らかにした自民党の高野光二郎参院議員について指摘した。 高野氏は昨年末に当時秘書だった20代男性を殴打し、鼻血を出させたと14日、国会内で記者団に説明。「飲み会で気合を入れるつもりで胸の辺りをたたくつもりが、鼻に当たった」と語っていた。 これについて濱田は")
 
