3ee04fb3cc607779689a9888ed0d202b174ebd8a
Aadeesh11 <asoswal@gmail.com>
2023-06-22 07:44:31 +0530

    remove unnecessary comments

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,24 +6,18 @@ use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    /**
-     * Find the article by its id.
-     */
     public function getArticle($id)
     {
         return Article::findOrFail($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Article $article, array $data)
+    public function update(Article $article, array $data): Article
     {
-        //update the article's data
-        //only those fields would be set, that are mass-assignable.
+        // only those fields would be set, that are mass-assignable.
         $article->update($data);
 
-        // Save the updated record
         $article->save();
+
+        return $article;
     }
 }
