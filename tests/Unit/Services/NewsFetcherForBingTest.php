45179617746f72bb43a2ee9b32917bec5e0e7af2
Tejas Lolage <tejaslolage@gmail.com>
2023-06-22 14:52:42 +0900

    Added tests for checking correct request and providers

--- tests/Unit/Services/NewsFetcherForBingTest.php
+++ tests/Unit/Services/NewsFetcherForBingTest.php
@@ -3,36 +3,38 @@
 namespace Tests\Unit\Services\NewsFetcher;
 
 use App\Services\NewsFetcher\NewsFetcherForBing;
-use Exception;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
-
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class NewsFetcherForBingTest extends TestCase
 {
     use DatabaseTransactions;
-    public function testFetch()
+
+    public function testFetch(): void
     {
         Http::fake([
-            'https://api.bing.microsoft.com/*' => Http::response(['data' => 'mocked data'], 200)
+            'https://api.bing.microsoft.com/*' => Http::response(['data' => 'mocked data'], 200),
         ]);
         $newsFetcher = new NewsFetcherForBing();
-        $response = $newsFetcher->fetch('search query', 10);
+        $response = $newsFetcher->fetch('', 10);
+        $this->testRequest();
         $this->assertEquals(['data' => 'mocked data'], $response);
     }
 
-    public function testFetchReturnsValidResponse()
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testFetchReturnsValidResponse(array $newsData): void
     {
-        $newsData = [
-            'articles' => [
-                ['title' => 'Sample News 1', 'link' => 'Sample news link 1'],
-                ['title' => 'Sample News 2', 'link' => 'Sample news link 2'],
-            ]
-        ];
-
         Http::fake([
-            'https://api.bing.microsoft.com/*' => Http::response($newsData, 200)
+            'https://api.bing.microsoft.com/*' => Http::response($newsData, 200),
         ]);
 
         $newsFetcher = new NewsFetcherForBing();
@@ -43,14 +45,42 @@ class NewsFetcherForBingTest extends TestCase
         $this->assertNotEmpty($response['articles']);
     }
 
-    public function testFetchThrowsExceptionOnError()
+    public function testFetchThrowsExceptionOnError(): void
     {
         Http::fake([
-            'https://api.bing.microsoft.com/*' => Http::response(['error' => 'Bing API returned an error: mocked error'], 500)
+            'https://api.bing.microsoft.com/*' => Http::response(['error' => 'Bing API returned an error: mocked error'], 500),
         ]);
-        $this->expectException(Exception::class);
+        $this->expectException(\Exception::class);
         $this->expectExceptionMessage('Bing API returned an error: mocked error');
         $newsFetcher = new NewsFetcherForBing();
-        $newsFetcher->fetch('search query', 10);
+        $newsFetcher->fetch('', 10);
+    }
+
+    private function testRequest(): void
+    {
+        Http::assertSent(function (Request $request) {
+            return $request->hasHeader('Ocp-Apim-Subscription-Key', config('services.bing.key'))
+                && $request->hasHeader('mkt', 'ja-JP')
+                && 'https://api.bing.microsoft.com/v7.0/news/search?q=&count=10&setLang=jp&freshness=Day&safeSearch=Off' == $request->url()
+                && '' == $request['q']
+                && '10' == $request['count']
+                && 'jp' == $request['setLang']
+                && 'Day' == $request['freshness']
+                && 'Off' == $request['safeSearch'];
+        });
+    }
+
+    private function dataProvider(): array
+    {
+        return [
+            [['articles' => [
+                ['title' => 'Sample News 1', 'link' => 'Sample news link 1'],
+                ['title' => 'Sample News 2', 'link' => 'Sample news link 2'],
+            ]]],
+            [['articles' => [
+                ['title' => 'Sample News 1', 'link' => 'Sample news link 1'],
+                ['title' => 'Sample News 2', 'link' => 'Sample news link 2'],
+            ]]],
+        ];
     }
 }
