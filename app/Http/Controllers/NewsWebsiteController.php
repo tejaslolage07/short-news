f0b651bfd68e382e7fbc5f17a55ee1a558181f1a
Aadeesh11 <asoswal@gmail.com>
2023-06-22 08:08:11 +0530

    remove comments.

--- app/Http/Controllers/NewsWebsiteController.php
+++ app/Http/Controllers/NewsWebsiteController.php
@@ -3,13 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\NewsWebsite;
-use Illuminate\Http\Request;
 
 class NewsWebsiteController extends Controller
 {
-    /**
-     * Find the article by its id.
-     */
     public function getNewsWebsiteFromID($id)
     {
         return NewsWebsite::findOrFail($id);
