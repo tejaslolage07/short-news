70c898bac34473ddf013fa0b8e3be05cfaff6435
Tejas Lolage <tejaslolage@gmail.com>
2023-06-22 11:48:55 +0900

    Implemented good coding practices

--- app/Services/NewsFetcher/NewsFetcherForNewsDataIo.php
+++ app/Services/NewsFetcher/NewsFetcherForNewsDataIo.php
@@ -3,43 +3,42 @@
 namespace App\Services\NewsFetcher;
 
 use Illuminate\Support\Facades\Http;
-use Exception;
 
 class NewsFetcherForNewsDataIo
 {
     private const url = 'https://newsdata.io/api/1/news';
+
     public function fetch(string $searchQuery = '', string $category = '', string $page = ''): array
     {
         $headers = $this->getHeaders();
         $params = $this->getParams($searchQuery, $category, $page);
-        $response = Http::withHeaders($headers)->get(self::url, $params);
-        if (!$response->successful()) {
-            throw new Exception('NewsDataIO API returned an error: ' . $response->body());
-        }
+        $response = Http::withHeaders($headers)->get(self::url, $params)->throw();
+
         return $response->json();
     }
 
     private function getHeaders(): array
     {
         return [
-            'X-ACCESS-KEY' => config('services.newsdataio.key')
+            'X-ACCESS-KEY' => config('services.newsdataio.key'),
         ];
     }
 
     private function getParams(string $searchQuery, string $category, string $page): array
     {
-        $params = array();
-        if($searchQuery !== '') {
+        $params = [];
+        if ('' !== $searchQuery) {
             $params['q'] = $searchQuery;
         }
-        if($category !== '') {
+        if ('' !== $category) {
             $params['category'] = $category;
         }
-        if($page !== '') {
+        if ('' !== $page) {
             $params['page'] = $page;
         }
         $params['language'] = 'jp';
         $params['country'] = 'jp';
+
         return $params;
     }
 }
