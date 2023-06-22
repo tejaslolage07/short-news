c23fd0cfe0b183fbaa7ffa8145f3e2bb58010e9b
Aadeesh11 <asoswal@gmail.com>
2023-06-22 07:34:42 +0530

    calculate max in another function

--- app/Services/Sockets/SummarizerSocket/ChatGptSummarizer.py
+++ app/Services/Sockets/SummarizerSocket/ChatGptSummarizer.py
@@ -10,12 +10,16 @@ load_dotenv()
 class ChatGptSummarizer:
     MODEL = "gpt-3.5-turbo"
 
+    def _get_max_tokens(self, num_tokens: int):
+        # 300 is set because summary should be short.
+        return min(
+            300, 2048-num_tokens, num_tokens)
+
     def _tokenized_prompt(self, text):
         encoding = tiktoken.get_encoding("cl100k_base")
         return encoding.encode(text)
 
     def _format_prompt(self, prompt: str):
-
         return prompt.strip()
 
     def _api_call(self, prompt, max_tokens: int, temperature: float):
@@ -43,8 +47,8 @@ class ChatGptSummarizer:
                 textPrompt)[0:maxInputTokens]
             num_tokens = len(tokenArray)
 
-            response = self._api_call(prompt=textPrompt, max_tokens=min(
-                300, 2048-num_tokens, num_tokens), temperature=0.0)
+            response = self._api_call(
+                prompt=textPrompt, max_tokens=self._get_max_tokens(num_tokens), temperature=0.0)
 
             summarizedText = response["choices"][0].message["content"]
             return summarizedText
