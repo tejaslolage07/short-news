081b5b9722b7ccfd3fd541b994100793ccbb9f1b
Tejas Lolage <tejaslolage@gmail.com>
2023-06-22 14:52:42 +0900

    Added tests for checking correct request and providers

--- tests/Unit/Services/NewsFetcherForNewsDataIoTest.php
+++ tests/Unit/Services/NewsFetcherForNewsDataIoTest.php
@@ -3,35 +3,37 @@
 namespace Tests\Unit\Services\NewsFetcher;
 
 use App\Services\NewsFetcher\NewsFetcherForNewsDataIo;
-use Exception;
+use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class NewsFetcherForNewsDataIoTest extends TestCase
 {
-    public function testFetch()
+    public function testFetch(): void
     {
         Http::fake([
-            'https://newsdata.io/*' => Http::response(['data' => 'mocked data'], 200)
+            'https://newsdata.io/*' => Http::response(['data' => 'mocked data'], 200),
         ]);
         $newsFetcher = new NewsFetcherForNewsDataIo();
         $response = $newsFetcher->fetch();
+        $this->testRequest();
         $this->assertEquals(['data' => 'mocked data'], $response);
     }
 
-    public function testFetchReturnsValidResponse()
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testFetchReturnsValidResponse(array $newsData): void
     {
-        $newsData = [
-            'articles' => [
-                ['title' => 'Sample News 1', 'link' => 'Sample news link 1'],
-                ['title' => 'Sample News 2', 'link' => 'Sample news link 2'],
-            ]
-        ];
-
         Http::fake([
-            'https://newsdata.io/*' => Http::response($newsData, 200)
+            'https://newsdata.io/*' => Http::response($newsData, 200),
         ]);
-        
+
         $newsFetcher = new NewsFetcherForNewsDataIo();
         $response = $newsFetcher->fetch();
         $this->assertEquals($newsData, $response);
@@ -39,16 +41,40 @@ class NewsFetcherForNewsDataIoTest extends TestCase
         $this->assertNotEmpty($response['articles']);
     }
 
-    public function testFetchThrowsExceptionOnError()
+    public function testFetchThrowsExceptionOnError(): void
     {
         Http::fake([
-            'https://newsdata.io/*' => Http::response(['error' => 'NewsDataIO API returned an error: mocked error'], 500)
+            'https://newsdata.io/*' => Http::response(['error' => 'NewsDataIO API returned an error: mocked error'], 500),
         ]);
 
-        $this->expectException(Exception::class);
+        $this->expectException(\Exception::class);
         $this->expectExceptionMessage('NewsDataIO API returned an error: mocked error');
 
         $newsFetcher = new NewsFetcherForNewsDataIo();
         $newsFetcher->fetch();
+    }
+
+    private function testRequest(): void
+    {
+        Http::assertSent(function (Request $request) {
+            return $request->hasHeader('X-ACCESS-KEY', config('services.newsdataio.key'))
+                && 'https://newsdata.io/api/1/news?language=jp&country=jp' == $request->url()
+                && 'jp' == $request['language']
+                && 'jp' == $request['country'];
+        });
+    }
+
+    private function dataProvider(): array
+    {
+        return [
+            [['articles' => [
+                ['title' => 'Sample News 1', 'link' => 'Sample news link 1'],
+                ['title' => 'Sample News 2', 'link' => 'Sample news link 2'],
+            ]]],
+            [['articles' => [
+                ['title' => 'Sample News 1', 'link' => 'Sample news link 1'],
+                ['title' => 'Sample News 2', 'link' => 'Sample news link 2'],
+            ]]],
+        ];
     }
 }
