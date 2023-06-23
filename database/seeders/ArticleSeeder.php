261a0472903cd30a402273a62a5dae93d709faf6
Aadeesh11 <asoswal@gmail.com>
2023-06-23 08:45:53 +0530

    add migration, seeder, del articleController.

---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\Article::factory(5)->create();
+    }
+}
