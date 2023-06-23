e9af26007be7863b430698b59fa9d0f8eb720111
Aadeesh11 <asoswal@gmail.com>
2023-06-23 10:57:06 +0530

    update the tests.

--- tests/Feature/SummarizerTest.php
+++ tests/Feature/SummarizerTest.php
@@ -2,45 +2,38 @@
 
 namespace Tests\Feature;
 
-use App\Http\Controllers\ArticleController;
 use App\Jobs\SummarizeArticle;
 use App\Models\Article;
 use App\Services\Sockets\Summarizer;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class SummarizerTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
-    /**
-     * A basic feature test example.
-     */
-    public function test_if_summary_is_generated_and_DB_updated(): void
+    public function testIfSummaryIsGeneratedAndDBUpdated(): void
     {
-        // seed the DB with dummy data
         $this->seed();
+        $articlesCount = Article::count();
 
-        // check if DB is seeded, 5 news websites and 10 articles
-        $this->assertDatabaseCount('articles', 10);
-        $this->assertDatabaseCount('news_websites', 5);
-
-        #prepare the article
-        $articleBody = "Dummy news article, pass this to summarizer and check if summary is generated and DB updated";
+        $articleBody = 'Dummy news article, pass this to summarizer and check if summary is generated and DB updated';
         $article = Article::factory(1)->create()->first();
         $this->assertModelExists($article);
-        $this->assertInstanceOf(Article::class, $article);
         $this->assertNotNull($article->id);
 
-        #handle the job
         $a = new SummarizeArticle($article, $articleBody, '', 1);
         $a->handle(new ArticleController(), new Summarizer());
         $this->assertNotEmpty($article->short_news);
 
-        #check if Job updated the DB
-        $this->assertDatabaseCount('articles', 11);
+        $this->assertDatabaseCount('articles', $articlesCount + 1);
         $this->assertDatabaseHas('articles', [
-            'short_news' => $article->short_news
+            'short_news' => $article->short_news,
         ]);
     }
 }
