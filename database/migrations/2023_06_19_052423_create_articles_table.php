3182344e246208717a11dfe426119b8990c0bb1d
Aadeesh11 <asoswal@gmail.com>
2023-06-23 07:46:36 +0530

    add: country, language, keywords, category cols

--- database/migrations/2023_06_19_052423_create_articles_table.php
+++ database/migrations/2023_06_19_052423_create_articles_table.php
@@ -4,25 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
+return new class() extends Migration {
     public function up(): void
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->text('short_news')->nullable(false);
-            $table->text('headline')->nullable(false);
-            $table->string('author', 256)->nullable(true);
-            //$table->foreignId('news_website_id')->nullable(true)->constrained();
-            $table->text('article_url')->nullable(false);
-            $table->text('image_url')->nullable(true);
-            $table->text('article_s3_filename')->nullable(false);
-            $table->timestamp('published_at')->nullable(false);
+            $table->text('short_news');
+            $table->text('headline');
+            $table->string('author', 256)->nullable();
+            $table->text('article_url', 512);
+            $table->text('image_url')->nullable();
+            $table->text('article_s3_filename');
+            $table->timestamp('published_at');
             $table->timestamps();
-            $table->timestamp('fetched_at')->nullable(false);
-            $table->enum('source', ['api', 'scraper'])->nullable(false);
+            $table->timestamp('fetched_at');
+            $table->enum('source', ['bingApi', 'newsDataIoApi', 'api', 'scraper']);
+            $table->text('country')->nullable();
+            $table->enum('language', ['ja', 'en'])->nullable();
+            $table->text('category')->nullable();
+            $table->text('keywords')->nullable();
         });
     }
 
