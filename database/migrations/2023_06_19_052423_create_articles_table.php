657e4b57b61cfbd2071c171cf2f7626265671f9f
Aadeesh11 <asoswal@gmail.com>
2023-06-23 08:45:53 +0530

    add migration, seeder, del articleController.

--- database/migrations/2023_06_19_052423_create_articles_table.php
+++ database/migrations/2023_06_19_052423_create_articles_table.php
@@ -12,20 +12,17 @@ return new class() extends Migration {
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->text('short_news')->nullable(false);
-            $table->text('headline')->nullable(false);
+            $table->text('short_news');
+            $table->text('headline');
             $table->string('author', 256)->nullable(true);
-            // This col will be added in the Job queue PR.
-            // $table->foreignId('news_website_id')->nullable(true)->constrained();
-            $table->text('article_url')->nullable(false);
+            $table->text('article_url');
             $table->text('image_url')->nullable(true);
-            $table->text('article_s3_filename')->nullable(false);
-            $table->timestamp('published_at')->nullable(false);
+            $table->text('article_s3_filename');
+            $table->timestamp('published_at');
             $table->timestamps();
-            $table->timestamp('fetched_at')->nullable(false);
-            $table->enum('source', ['api', 'scraper'])->nullable(false);
+            $table->timestamp('fetched_at');
+            $table->enum('source', ['api', 'scraper']);
             $table->text('country')->nullable(true);
-            // https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes for lang codes
             $table->enum('language', ['ja', 'en'])->nullable(true);
             $table->text('category')->nullable(true);
             $table->text('keywords')->nullable(true);
