2ac27c55d01344a15e9e52b764fe3851b2ecec4f
Aadeesh11 <66922161+Aadeesh11@users.noreply.github.com>
2023-06-29 06:50:18 +0530

    Remove comments

--- database/migrations/2023_06_19_052423_create_news_websites_table.php
+++ database/migrations/2023_06_19_052423_create_news_websites_table.php
@@ -7,9 +7,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class() extends Migration {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::create('news_websites', function (Blueprint $table) {
@@ -19,9 +17,6 @@ return new class() extends Migration {
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('news_websites');
