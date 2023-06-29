6582b58659bb15bc8edc4fe3f3a925d533f3e98e
Aadeesh11 <asoswal@gmail.com>
2023-06-29 15:04:28 +0530

    requested changes.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,7 +10,6 @@ class ArticleController extends Controller
     public function index(Request $request)
     {
         return Article::with('newsWebsite')
-            // ->select('id', 'short_news', 'headline', 'author', 'article_url', 'image_url', 'published_at', 'news_website_id')
             ->where('short_news', '!=', '', 'and')
             ->where('news_website_id', '!=', null)
             ->orderByDesc('published_at')
