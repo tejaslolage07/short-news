16b6d090ed400e39b6557121c277c4532df585c6
Aadeesh11 <asoswal@gmail.com>
2023-06-29 15:05:59 +0530

    rm comments

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -16,11 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-/*
- * Endpoint to get news articles! Pagination is cursor based.
- */
 Route::get('/news', [ArticleController::class, 'index']);
