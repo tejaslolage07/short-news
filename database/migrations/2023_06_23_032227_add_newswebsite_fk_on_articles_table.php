d5ff74d09b6d522936e6879bbca6b4fb87a587b6
Aadeesh11 <66922161+Aadeesh11@users.noreply.github.com>
2023-06-29 06:50:36 +0530

    remove comments

--- database/migrations/2023_06_23_032227_add_newswebsite_fk_on_articles_table.php
+++ database/migrations/2023_06_23_032227_add_newswebsite_fk_on_articles_table.php
@@ -5,9 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class() extends Migration {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         if (Schema::hasTable('articles')) {
@@ -17,9 +15,6 @@ return new class() extends Migration {
         }
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         if (Schema::hasTable('articles')) {
