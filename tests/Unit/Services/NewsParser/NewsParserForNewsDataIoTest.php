a8b46b5ffc505da54b552720c8af97671fc6141c
Tejas Lolage <tejaslolage@gmail.com>
2023-06-29 16:43:39 +0900

    Corrected wrong conflict changes

--- tests/Unit/Services/NewsParser/NewsParserForNewsDataIoTest.php
+++ tests/Unit/Services/NewsParser/NewsParserForNewsDataIoTest.php
@@ -15,20 +15,19 @@ class NewsParserForNewsDataIoTest extends TestCase
     /**
      * @dataProvider responseProvider
      */
-    public function testGetParsedData(array $response): void
+    public function testGetParsedData(array $mockedResponse, array $expectedParsedResult): void
     {
         $parser = new NewsParserForNewsDataIo();
-        $parsedData = $parser->getParsedData($response);
+        $parsedData = $parser->getParsedData($mockedResponse);
         foreach ($parsedData as $index => $article) {
-            $this->assertValidParsedArticle(parsedArticle: $article, mockedArticle: $response['results'][$index]);
+            $this->assertValidParsedArticle($article, $expectedParsedResult[$index]);
         }
     }
 
-    private function assertValidParsedArticle(array $parsedArticle, array $mockedArticle): void
+    private function assertValidParsedArticle(array $parsedArticle, array $expectedParsedArticle): void
     {
         $this->assertValidParsedArticleKeys($parsedArticle);
-        $this->assertValidParsedArticleData(parsedArticle: $parsedArticle, mockedArticle: $mockedArticle);
-        $this->assertValidParsedArticleDateTimeFormats($parsedArticle);
+        $this->assertValidParsedArticleData($parsedArticle, $expectedParsedArticle);
     }
 
     private function assertValidParsedArticleKeys(array $parsedArticle): void
@@ -43,35 +42,16 @@ class NewsParserForNewsDataIoTest extends TestCase
         $this->assertArrayHasKey('fetched_at', $parsedArticle);
     }
 
-    private function assertValidParsedArticleData(array $parsedArticle, array $mockedArticle): void
+    private function assertValidParsedArticleData(array $parsedArticle, array $expectedParsedArticle): void
     {
-        $this->assertEquals($mockedArticle['title'], $parsedArticle['headline']);
-        $this->assertEquals($mockedArticle['link'], $parsedArticle['article_url']);
-        $this->assertEquals($mockedArticle['source_id'], $parsedArticle['news_website']);
-        $this->assertEquals($mockedArticle['content'], $parsedArticle['content']);
-        $this->assertEquals($mockedArticle['image_url'], $parsedArticle['image_url']);
-        $this->assertEquals($this->getAuthorFromMockedData($mockedArticle), $parsedArticle['author']);
-    }
-
-    private function assertValidParsedArticleDateTimeFormats(array $parsedArticle): void
-    {
-        $this->assertDateTimeFormat($parsedArticle['published_at']);
-        $this->assertDateTimeFormat($parsedArticle['fetched_at']);
-    }
-
-    private function getAuthorFromMockedData(array $mockedArticle): ?string
-    {
-        return $mockedArticle['creator'][0] ?? null;
-    }
-
-    private function assertDateTimeFormat(?string $dateTimeString): void
-    {
-        if (!$dateTimeString) {
-            return;
-        }
-        $dateTime = \DateTime::createFromFormat('Y-m-d H:i:s', $dateTimeString);
-        $this->assertInstanceOf(\DateTime::class, $dateTime);
-        $this->assertEquals($dateTimeString, $dateTime->format('Y-m-d H:i:s'));
+        $this->assertEquals($parsedArticle['headline'], $expectedParsedArticle['headline']);
+        $this->assertEquals($parsedArticle['article_url'], $expectedParsedArticle['article_url']);
+        $this->assertEquals($parsedArticle['news_website'], $expectedParsedArticle['news_website']);
+        $this->assertEquals($parsedArticle['content'], $expectedParsedArticle['content']);
+        $this->assertEquals($parsedArticle['image_url'], $expectedParsedArticle['image_url']);
+        $this->assertEquals($parsedArticle['author'], $expectedParsedArticle['author']);
+        $this->assertEquals($parsedArticle['published_at'], $expectedParsedArticle['published_at']);
+        $this->assertEquals($parsedArticle['fetched_at'], $expectedParsedArticle['fetched_at']);
     }
 
     private function responseProvider(): array
@@ -119,6 +99,20 @@ class NewsParserForNewsDataIoTest extends TestCase
                     ],
                 ],
                 [
+                    [
+                        'headline' => 'Article 2',
+                        'article_url' => 'https://example.com/article2',
+                        'author' => null,
+                        'content' => 'Article 2 content',
+                        'image_url' => 'https://example.com/image2.jpg',
+                        'news_website' => 'full_count',
+                        'published_at' => '2023-06-20 04:19:47',
+                        'fetched_at' => date('Y-m-d H:i:s'),
+                    ],
+                ],
+            ],
+            [
+                [
                     'results' => [
                         [
                             'title' => 'Article 3',
@@ -131,9 +125,20 @@ class NewsParserForNewsDataIoTest extends TestCase
                         ],
                     ],
                     'nextPage' => null,
-                ]
+                ],
+                [
+                    [
+                        'headline' => 'Article 3',
+                        'article_url' => 'https://example.com/article3',
+                        'author' => null,
+                        'content' => 'Article 3 content',
+                        'image_url' => null,
+                        'news_website' => 'full_count',
+                        'published_at' => null,
+                        'fetched_at' => date('Y-m-d H:i:s'),
+                    ],
+                ],
             ],
         ];
     }
 }
-    
