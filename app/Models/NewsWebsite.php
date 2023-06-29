8f0591dd22569252a96be1bf678d94a6ab41660a
Aadeesh11 <66922161+Aadeesh11@users.noreply.github.com>
2023-06-29 06:49:08 +0530

    Update NewsWebsite.php

--- app/Models/NewsWebsite.php
+++ app/Models/NewsWebsite.php
@@ -9,11 +9,6 @@ class NewsWebsite extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'website',
     ];
