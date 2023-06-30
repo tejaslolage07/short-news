c77e85beba05bd1f45bf1ab0d6788193d60b10f3
Tejas Lolage <tejaslolage@gmail.com>
2023-06-30 14:32:55 +0900

    Updated name of NewsFetcher to ChunkFetcher for NewsDataIo

--- app/Services/NewsHandler/NewsFetcher/ChunkFetcherForNewsDataIo.php
+++ app/Services/NewsHandler/NewsFetcher/ChunkFetcherForNewsDataIo.php
@@ -2,72 +2,46 @@
 
 namespace App\Services\NewsHandler\NewsFetcher;
 
-use App\Models\Article;
-use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
 
 class ChunkFetcherForNewsDataIo
 {
-    public function chunkFetchNewsFromNewsDataIo(NewsFetcherForNewsDataIo $newsFetcher): array
+    private const URL = 'https://newsdata.io/api/1/news';
+
+    public function chunkFetch(string $searchQuery = '', string $category = '', string $page = ''): array
     {
-        try {
-            $responses = $this->getChunkOfResponses($newsFetcher);
-        } catch (\Exception $e) {
-            report('An error occurred: '.$e);
+        $headers = $this->getHeaders();
+        $params = $this->getParams($searchQuery, $category, $page);
+        $response = Http::withHeaders($headers)
+            ->get(self::URL, $params)
+            ->throw()
+        ;
+
+        return $response->json();
+    }
+
+    private function getHeaders(): array
+    {
+        return [
+            'X-ACCESS-KEY' => config('services.newsdataio.key'),
+        ];
+    }
+
+    private function getParams(string $searchQuery, string $category, string $page): array
+    {
+        $params = [];
+        if ('' !== $searchQuery) {
+            $params['q'] = $searchQuery;
         }
-
-        return $responses;
-    }
-
-    private function getChunkOfResponses(NewsFetcherForNewsDataIo $newsFetcher): array
-    {
-        $existingUrl = $this->getLatestUrlFromDB();
-        $dateTimeCap = $this->getDaysCap(1);    // Give negative values for no cap: (Warning: All credits could get used in one session.)
-        $page = '';
-        $creditsUsed = 0;
-        $responses = [];
-        do {
-            $fetchedNews = $newsFetcher->fetch('', '', $page);
-            ++$creditsUsed;
-
-            for ($i = 0; $i < 10; ++$i) {
-                $articleUrl = $fetchedNews['results'][$i]['link'];
-                $articlePublishedAt = $fetchedNews['results'][$i]['pubDate'];
-                if (!$this->isNewArticle($existingUrl, $dateTimeCap, $articleUrl, $articlePublishedAt)) {
-                    $slicedArray['results'] = array_slice($fetchedNews['results'], 0, $i);
-                    $responses[] = json_encode($slicedArray);
-
-                    break 2;
-                }
-            }
-            $responses[] = json_encode($fetchedNews);
-            $page = $fetchedNews['nextPage'];
-        } while ($page);
-        info(Carbon::now()->addHour(9)->format('Y-m-d H:i:s')."\tTotal credits used in this session: ".$creditsUsed."\n");
-
-        return $responses;
-    }
-
-    private function getLatestUrlFromDB(): string
-    {
-        $latestUrl = Article::orderBy('published_at', 'desc')->value('article_url');
-
-        return $latestUrl ?: '';
-    }
-
-    private function getDaysCap(int $days): string
-    {
-        $existingArticleDateTime = Article::orderBy('published_at', 'desc')->value('published_at');
-        $daysCap = Carbon::now()->subDays($days)->tz('UTC')->format('Y-m-d H:i:s');
-        dump($daysCap);
-        if ($existingArticleDateTime && Carbon::parse($daysCap) < Carbon::parse($existingArticleDateTime)) {
-            return $existingArticleDateTime;
+        if ('' !== $category) {
+            $params['category'] = $category;
         }
+        if ('' !== $page) {
+            $params['page'] = $page;
+        }
+        $params['language'] = 'jp';
+        $params['country'] = 'jp';
 
-        return $daysCap;
-    }
-
-    private function isNewArticle(string $existingUrl, string $dateTimeCap, string $articleUrl, string $articlePublishedAt): bool
-    {
-        return $existingUrl !== $articleUrl && $articlePublishedAt > $dateTimeCap;
+        return $params;
     }
 }
