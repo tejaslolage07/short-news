e1cf775a2262341cbe2550e7db60d8734d2b7e22
Tejas Lolage <tejaslolage@gmail.com>
2023-06-30 13:53:55 +0900

    Removed tests for Bing api

--- tests/Feature/Services/NewsHandlerTest.php
+++ tests/Feature/Services/NewsHandlerTest.php
@@ -4,6 +4,9 @@ namespace Tests\Unit\Services;
 
 use App\Models\Article;
 use App\Services\NewsHandler\NewsHandler;
+use App\Services\NewsHandler\NewsFetcher\NewsFetcherForNewsDataIo;
+use App\Services\NewsHandler\NewsFetcher\ChunkFetcherForNewsDataIo;
+use App\Services\NewsHandler\NewsParser\NewsParserForNewsDataIo;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
@@ -17,26 +20,16 @@ class NewsHandlerTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function testFetchAndStoreNewsFromBing()
-    {
-        Queue::fake();
-        $initialQueueSize = Queue::size();
-        $initialDatabaseCount = Article::count();
-        $service = new NewsHandler();
-        $service->fetchAndStoreNewsFromBing();
-        $finalQueueSize = Queue::size();
-        $finalDatabaseCount = Article::count();
-        $this->assertGreaterThan($initialQueueSize, $finalQueueSize);
-        $this->assertGreaterThan($initialDatabaseCount, $finalDatabaseCount);
-    }
-
     public function testFetchAndStoreNewsFromNewsDataIo()
     {
+        $newsFetcher = new NewsFetcherForNewsDataIo;
+        $newsParser = new NewsParserForNewsDataIo;
+        $chunkFetcher = new ChunkFetcherForNewsDataIo;
         Queue::fake();
         $initialQueueSize = Queue::size();
         $initialDatabaseCount = Article::count();
         $service = new NewsHandler();
-        $service->fetchAndStoreNewsFromNewsDataIo();
+        $service->fetchAndStoreNewsFromNewsDataIo($newsFetcher, $newsParser, $chunkFetcher);
         $finalQueueSize = Queue::size();
         $finalDatabaseCount = Article::count();
         $this->assertGreaterThan($initialQueueSize, $finalQueueSize);
