936e2365424c84505982ac9925ae8fd0f2ebd5c3
Tejas Lolage <tejaslolage@gmail.com>
2023-07-03 12:03:23 +0900

    Deleted Bing tests

--- app/Console/Commands/FetchNewsFromNewsDataIo.php
+++ app/Console/Commands/FetchNewsFromNewsDataIo.php
@@ -13,16 +13,15 @@ class FetchNewsFromNewsDataIo extends Command
      * @var string
      */
     protected $signature = 'fetch:newsdataio
-                            {untilDate? : The date until which news articles will be fetched (format: Y-m-d))}';
+                            {untilDate? : The date until which news articles will be fetched (format: YYYY-MM-DD))}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Fetch, summarize and store news from NewsData.io
-                                (if no date is provided, null is considered.)
-                                Arguments: untilDate=YYYY-MM-DD';
+    protected $description = 'Fetch, summarize and store news from NewsData.io. Arguments: untilDate=YYYY-MM-DD
+                                (if no date is provided, null is considered.)';
 
     /**
      * Execute the console command.
