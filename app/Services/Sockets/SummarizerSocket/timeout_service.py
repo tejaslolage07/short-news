22e4306717c77d7b83cc82726d69b5a0bcb04bd5
Aadeesh11 <asoswal@gmail.com>
2023-07-04 08:26:36 +0530

    add types

--- app/Services/Sockets/SummarizerSocket/timeout_service.py
+++ app/Services/Sockets/SummarizerSocket/timeout_service.py
@@ -1,17 +1,17 @@
 import signal
 
 class TimeOutService:
-    def __init__(self, timeout, error_msg_on_timeout="Timed out!"):
+    def __init__(self, timeout : int, error_msg_on_timeout="Timed out!"):
         self.timeout = timeout
         self.error_msg = error_msg_on_timeout
 
 
-    def handle_timeout(self, signum, frame):
+    def handle_timeout(self, signum, frame) -> None:
         raise TimeoutError(self.error_msg)
 
-    def set_timeout(self):
+    def set_timeout(self) -> None:
         signal.signal(signal.SIGALRM, self.handle_timeout)
         signal.alarm(self.timeout)
 
-    def clear_timeout(self):
+    def clear_timeout(self) -> None:
         signal.alarm(0)
--- app/Services/Sockets/SummarizerSocket/tokenizer_service.py
+++ app/Services/Sockets/SummarizerSocket/tokenizer_service.py
@@ -3,7 +3,7 @@ import tiktoken
 class TokenizerService:
     
     @staticmethod
-    def limit_tokens(prompt, limit) -> str:
+    def limit_tokens(prompt, limit : int) -> str:
         encoding = tiktoken.get_encoding("cl100k_base")
         tokenized_prompt = encoding.encode(prompt)
         limited_tokenized_prompt = tokenized_prompt[0:limit]
