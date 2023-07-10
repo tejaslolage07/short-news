9f290ffb0f71498c94f060d73580963cf15f88a2
RoySoumyadeep <soumya96roy@gmail.com>
2023-07-10 13:59:42 +0900

    Update logic

--- tests/Feature/ArticleReadTest.php
+++ tests/Feature/ArticleReadTest.php
@@ -193,17 +193,17 @@ class ArticleReadTest extends TestCase
             $currentArticle = $articles[$x];
             $nextArticle = $articles[$x + 1];
 
-            $this->assertLessThanOrEqual($currentArticle['fetched_at'], $nextArticle['fetched_at']);
+            $this->assertLessThanOrEqual($nextArticle['fetched_at'], $currentArticle['fetched_at']);
             if ($currentArticle['fetched_at'] < $nextArticle['fetched_at']) {
                 continue;
             }
 
-            $this->assertLessThanOrEqual($currentArticle['published_at'], $nextArticle['published_at']);
+            $this->assertLessThanOrEqual($nextArticle['published_at'], $currentArticle['published_at']);
             if ($currentArticle['published_at'] < $nextArticle['published_at']) {
                 continue;
             }
 
-            $this->assertGreaterThan($currentArticle['id'], $nextArticle['id']);
+            $this->assertGreaterThan($nextArticle['id'], $currentArticle['id']);
         }
     }
 }
