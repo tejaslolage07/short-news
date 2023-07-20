f1fa2475635985234083f8becff070847e4ff682
Tejas Lolage <tejaslolage@gmail.com>
2023-07-20 17:33:08 +0900

    Refactored ExportAnalyticsData

--- app/Services/ExportAnalyticsData.php
+++ app/Services/ExportAnalyticsData.php
@@ -1,30 +1,42 @@
-<?php 
+<?php
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
 
 class ExportAnalyticsData
 {
-    public function exportCsv()
+    public const DIR = '';
+
+    public function exportCsv(): void
     {
-        $fileName = 'exported_data.csv';
+        $fileName = self::DIR.'analytics.csv';
         $articles = DB::table('articles')->get();
 
         $csvExporter = \League\Csv\Writer::createFromPath(storage_path($fileName), 'w+');
         $csvExporter->insertOne(['article_id', 'original_article_chars', 'summarized_article_chars']);
 
         foreach ($articles as $row) {
-            $s3Filename = $row->article_s3_filename;
-            $originalArticle = Storage::disk('s3')->get($s3Filename);
-            $summarizedArticle = $row->short_news;
-            $originalArticleChars = strlen($originalArticle);
-            $summarizedArticleChars = strlen($summarizedArticle);
-            $csvExporter->insertOne([$row->id, $originalArticleChars, $summarizedArticleChars]);
+            $originalArticleLength = $this->getOriginalArticleLength($row->article_s3_filename);
+            $summarizedArticleLength = $this->getSummarizedArticleLength($row->short_news);
+
+            $csvExporter->insertOne([$row->id, $originalArticleLength, $summarizedArticleLength]);
         }
 
-        return Response::download(storage_path($fileName))->deleteFileAfterSend(true);
+        Response::download(storage_path($fileName))->deleteFileAfterSend(true);
+    }
+
+    private function getSummarizedArticleLength(string $summarizedArticle): int
+    {
+        return strlen($summarizedArticle);
+    }
+
+    private function getOriginalArticleLength(string $s3Filename): int
+    {
+        $originalArticle = Storage::disk('s3')->get($s3Filename);
+
+        return strlen($originalArticle);
     }
 }
